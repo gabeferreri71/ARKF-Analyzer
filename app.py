048cc8b27efe2d3d5d085efec6e41f2db728ba4c
gabeferreri71 <95319421+gabeferreri71@users.noreply.github.com>
2022-02-14 18:58:38 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
+import pandas as pd 
+import numpy as np
+import questionary
+import fire 
+
 import utils.ARKFconnect as ARKFconnect
 tickers = ARKFconnect.relevantdf['ticker'].to_list()
+
+
+analysis_ticker = questionary.select("Which ticker from ARKF would you like to analyze?", tickers).ask()
