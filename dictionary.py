504f3541da42d1a7f7a571028f1018fbccc12211
Tyler <tycastleberry@gmail.com>
2022-02-15 09:20:46 -0600

    updated functions and loop

--- dictionary.py
+++ dictionary.py
@@ -1,39 +1,8 @@
-"""
-json_entry ={}
-
-json_entry["ticker"] = {
-    "ticker": "AAPL",
-    "2/9/2022" : {
-        "date": '5',
-        "beta": "1",
-        'sharpe': "1"
-        },
-    "2/10/2022" : {
-        "date": '10',
-        "beta": "2",
-        'sharpe': "1"
-        },
-}
-betas = []
-dates =[]
-for item in json_entry:
-    if item != 'ticker':
-        betas.append(json_entry[item]['beta'])
-print(betas)
-
-"""
-
-
 from pathlib import Path
 import json
 
 path = Path("./data/data.json")
-json_entry = {
-    "Ticker" : ticker,
-    "Date" : time,
-    "Beta" : beta,
-    "Sharpe" : sharpe
-}
+
 # data_for_json = {
 #         "analysis_ticker": analysis_ticker,
 #         "fund_ticker": fund_ticker,
@@ -43,9 +12,10 @@ json_entry = {
 #         "result": result,
 #         "date of analysis": first_date
 #     }
-def pushJson(path, json_entry):
+     
+def pushJson(path, data_for_json):
     with open(path, 'w') as outfile:
-       response = json.load(json_entry, outfile)
+       response = json.load(data_for_json, outfile)
 
 
 def pullJson(path, response):
@@ -55,9 +25,11 @@ def pullJson(path, response):
 data = pullJson(path, response)
 ticker_of_interest = "COIN"
 just_ticker = {}
-for item in json:
+for item in data:
     # look for ticker
     # add item to just_ticker
+    if analysis_ticker == ticker_of_interest:
+        just_ticker.append(analysis_ticker)
 
 
 
